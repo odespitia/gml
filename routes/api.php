62ac831fd015ac90a7aa56c7aec053f13e1e93e7
Oscar Espitia <oscare@etraining.co>
2021-10-11 23:54:34 -0500

    Configuracion de JWT

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'Admin/AuthController@login');
+    Route::post('logout', 'Admin/AuthController@logout');
+    Route::post('refresh', 'Admin/AuthController@refresh');
+    Route::post('me', 'Admin/AuthController@me');
+
 });
